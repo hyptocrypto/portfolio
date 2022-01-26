f50c0b56fcd0811ffcbc649f67998fbdfd62b37a
Julian Baumgartner <jbaumgartner93@gmail.com>
2022-01-26 09:51:43 -0500

    try to force darkmode default

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Page from "../components/page";
 import NavMenu from "../components/nav";
 import theme from "../components/theme";
-import { ChakraProvider, ColorModeScript} from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, useColorMode} from "@chakra-ui/react";
 import '@fontsource/raleway/700.css'
 import '@fontsource/open-sans/800.css'
 import Footer from "../components/footer";
